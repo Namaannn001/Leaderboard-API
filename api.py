008cb55ce8107da0c6abf868a71c6cd1448aec81
Namaannn001 <123639934+Namaannn001@users.noreply.github.com>
2025-02-17 12:24:31 +0530

    api.py

--- api.py
+++ api.py
@@ -34,6 +34,7 @@ def get_leaderboard():
     
     return jsonify(sorted_leaderboard)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
